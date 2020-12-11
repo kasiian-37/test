5e6f626f9d3a29833269bbb7b8a3c0e2edc00135
kasiian irina <karamio09@gmail.com>
2020-12-11 15:43:26 +0200

    Update apiService.js

---
+++ src/js/apiService.js
@@ -0,0 +1,33 @@
+const BASE_URL = 'https://callboard-backend.herokuapp.com/';
+
+//регистрация
+
+export default async function addregisterUser() {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataRegistry),
+    };
+
+    const response = await fetch(`${BASE_URL}auth/register`,options);
+  const responseJson = await response.json();
+  return responseJson
+  }
+
+  //авторизация
+  export default async function fetchAuthorisLogin(BASE_URL, dataRegistry) {
+    const options = {
+  method: 'POST',
+  body: JSON.stringify(dataRegistry),
+  headers: {
+    'Content-Type': 'application/json',
+    'accept': 'application/json',
+  },
+}
+    const response = await fetch(`${BASE_URL}auth/login`,options);
+    const responseJson = await response.json();
+    return  localStorage.setItem('key', `${responseJson.accessToken}`);
+
+}; 
