fbd600039d6cd5ab1fc44ad4c081ec52afa0615d
kasiian irina <karamio09@gmail.com>
2020-12-18 01:25:25 +0200

    Update authApi.js

--- src/js/auth-modal/authApi.js
+++ src/js/auth-modal/authApi.js
@@ -1,15 +1,45 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://callboard-backend.herokuapp.com';
+export const BASE_URL = 'https://callboard-backend.herokuapp.com';
 
 export const registerUser = userData => {
   const { email, password } = userData;
 
-  return axios.post(`https://callboard-backend.herokuapp.com/auth/register`, { email, password });
+return axios.post(`${BASE_URL}/auth/register`, { email, password });
 }
 
 export const loginUser = userData => {
   const { email, password } = userData;
 
-  return axios.post(`https://callboard-backend.herokuapp.com/users/login`, {email, password });
+  return axios.post(`${BASE_URL}/users/login`, {email, password });
 }
+
+
+// export async function fetchRegistration(url, dataRegistry) {
+//   const options = {
+// method: 'POST',
+// body: JSON.stringify(dataRegistry),
+// headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+    
+// },
+// }
+//   const response = await fetch(`${url}auth/register`,options);
+//   const responseJson = await response.json();
+//   return responseJson
+// };
+
+export async function fetchAuthenticationLogin(url, dataRegistry) {
+  const options = {
+method: 'POST',
+body: JSON.stringify(dataRegistry),
+headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'accept': 'application/json',
+},
+}
+  const response = await fetch(`${url}auth/login`,options);
+  const responseJson = await response.json();
+  return  responseJson
+
+};
