90e69561260100b700a5176a6ff4608cff8997ab
kasiian irina <karamio09@gmail.com>
2020-12-18 01:25:27 +0200

    Update authLogin.js

--- src/js/auth-modal/authLogin.js
+++ src/js/auth-modal/authLogin.js
@@ -1,8 +1,8 @@
 import { loginUser } from './authApi';
 import { pushError } from './pnotify';
 
-const loginFormRef = document.querySelector('#login');
-const submitHandler = async event => {
+const loginFormRef = document.querySelector('auth-form');
+export const submitHandler = async event => {
   event.preventDefault();
   
   const { currentTarget: form } = event;
@@ -26,6 +26,51 @@ const submitHandler = async event => {
       'Помилка!',
     );
   }
+  
+  
 }
 
 loginFormRef.addEventListener('submit', submitHandler)
+
+export default async function loginSubmit(log) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(log),
+  };
+
+  try {
+    await fetch(
+      'https://callboard-backend.herokuapp.com/auth/login',
+      requestOptions,
+    )
+      .then(response => response.json())
+      .then(result => {
+        if (result.message === 'Password is wrong') {
+          pushError(result.message);
+        } else if (
+          result.message ===
+          `User with ${
+            JSON.parse(requestOptions.body).email
+          } email doesn't exist`
+        ) {
+          pushError(result.message);
+        } else if (result.message === '"email" is not allowed to be empty') {
+          pushError(result.message);
+        } else if (result.message === '"password" is not allowed to be empty') {
+          pushError(result.message);
+        } else {
+          sessionStorage.setItem('token', result.accessToken);
+          sessionStorage.setItem('sid', result.sid);
+          sessionStorage.setItem('refresh', result.refreshToken);
+          // document.getElementById('auth-form').reset();
+          return result.user;
+        }
+      })
+     
+  } catch (error) {
+    console.log('error', error);
+  }
+}
